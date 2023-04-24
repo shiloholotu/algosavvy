2ea994ca74dceff1caf9ae66e745b07eb29e52e9
Shiloh Olotu <shiloholotu@gmail.com>
2023-04-24 16:07:00 -0500

    Add file-reading functionality

---
+++ scripts/file-reading.js
@@ -0,0 +1,11 @@
+
+function loadMdFile(text){
+	const r = mdToHTML(text);
+	document.getElementById("pageView").innerHTML = r[0];
+	eval(r[1]);//😱😱
+}
+async function readMdFile(location){
+    const response = await fetch(location);
+    const text = await response.text();
+    loadFile(text);
+}
--- scripts/util.js
+++
@@ -1,5 +0,0 @@
-async function readFile(location){
-    const response = await fetch(location);
-    const text = await response.text();
-    return text;
-  }
