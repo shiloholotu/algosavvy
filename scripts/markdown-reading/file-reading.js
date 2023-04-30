20b6adb9a1dc0d791c8007dafd19476eadec030a
Shiloh Olotu <shiloholotu@gmail.com>
2023-04-30 16:33:01 -0500

    Add ability to scroll to certain section of markdown

--- scripts/markdown-reading/file-reading.js
+++ scripts/markdown-reading/file-reading.js
@@ -1,11 +1,12 @@
 
-function loadMdFile(text){
+function loadMdFile(text,scrollTo){
 	const r = mdToHTML(text);
 	document.getElementById("pageView").innerHTML = r[0];
 	eval(r[1]);//😱😱
+    if(scrollTo != null)document.getElementById(scrollTo).scrollIntoView();
 }
-async function readMdFile(location){
+async function readMdFile(location,scrollTo = null){
     const response = await fetch(location);
     const text = await response.text();
-    loadMdFile(text);
+    loadMdFile(text,scrollTo);
 }
