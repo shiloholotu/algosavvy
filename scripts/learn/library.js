a0e31aa480bdf355b474f3f96fbaa848c1e50879
Shiloh Olotu <shiloholotu@gmail.com>
2023-04-30 20:15:56 -0500

    Add functionality for choosing doc by parsing url

---
+++ scripts/learn/library.js
@@ -0,0 +1,10 @@
+const docs = {
+    "introduction":[
+        //[md file, last update mm/dd/yyyy]
+        ["a-warm-hello.md","04/30/2023"]
+    ],
+    "easy":[],
+    "medium":[],
+    "hard":[],
+    "advanced":[]
+};
---
+++ scripts/learn/url-parsing.js
@@ -0,0 +1,13 @@
+function loadMdFromUrl(){
+    const url = window.location.href;
+    if(url.search("#") == -1)return 0;
+
+    const loc = url.split("#")[1].split("/");
+    
+    const folder = loc[0];
+    const doc = loc[1];
+    let section = null;
+    if(loc.length == 3)section = loc[2];
+
+    readMdFile(docs[folder][0],section);
+}
