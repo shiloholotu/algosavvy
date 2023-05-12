5f833f00128251fe15f409df0ec8e1f54084fc8d
Shiloh Olotu <shiloholotu@gmail.com>
2023-05-11 21:20:48 -0500

    Fix typo

--- scripts/learn/table-of-contents/generate-toc.js
+++ scripts/learn/table-of-contents/generate-toc.js
@@ -3,7 +3,7 @@ function generateTOC(){
     table.innerHTML = "";
     table.innerHTML += "<h2>Introduction</h2>";
     for(let i in docs["introduction"]){
-        table.innerHTML += `<a href="learn#introduction/${i}">${docs["introduction"][i][0]}<a><br>`;
+        table.innerHTML += `<a href="learn#introduction/${i}">${docs["introduction"][i][0]}</a><br>`;
     }
 
 }
