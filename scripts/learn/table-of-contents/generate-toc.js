97a338363df553dd47e7f63e5c0b798d0bae6d0a
Shiloh Olotu <shiloholotu@gmail.com>
2023-05-13 09:46:30 -0500

    Style: Remove box shadow from completed docs

--- scripts/learn/table-of-contents/generate-toc.js
+++ scripts/learn/table-of-contents/generate-toc.js
@@ -6,12 +6,14 @@ function generateTOC(){
 
         //completion status
         let compStatusStr = "<p style='opacity:.5;font-weight:lighter'><img src='assets/svg/minus.svg'>Incomplete</p>";
+        let boxShadowStr = "";
         if(getCompletionStatus(docs["introduction"][i][0]) == "complete"){
             compStatusStr = "<p style='opacity:.5;font-weight:lighter'><img src='assets/svg/check.svg' style='background:rgb(0,200,50,0.5)'>Complete</p>";
+            boxShadowStr = "style='box-shadow:none'";
         }
         table.innerHTML += `
         <a href="learn#introduction/${i}">
-            <div class='tocBlock'>
+            <div class='tocBlock' ${boxShadowStr}>
                 ${compStatusStr}
                 <h3>${docs["introduction"][i][0]}</h3>
                 <p> Last Updated: ${docs["introduction"][i][2]}</p>
