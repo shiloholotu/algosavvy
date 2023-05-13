66fbc73c2845c352cc8a99447fe89e54a3b0fb4f
Shiloh Olotu <shiloholotu@gmail.com>
2023-05-12 23:49:37 -0500

    Implement local storage history when displaying table of contents

--- scripts/learn/table-of-contents/generate-toc.js
+++ scripts/learn/table-of-contents/generate-toc.js
@@ -3,6 +3,12 @@ function generateTOC(){
     table.innerHTML = "";
     table.innerHTML += "<h2>INTRODUCTION</h2>";
     for(let i in docs["introduction"]){
+
+        //completion status
+        let compStatusStr = "<p style='opacity:.5'><img src='assets/svg/minus.svg'>Incomplete</p>";
+        if(getCompletionStatus(docs["introduction"][i][0]) == null){
+            compStatusStr = "<p style='opacity:.5'><img src='assets/svg/check.svg' style='background:rgb(0,200,50,0.5)'>Complete</p>";
+        }
         table.innerHTML += `
         <a href="learn#introduction/${i}">
             <div class='tocBlock'>
