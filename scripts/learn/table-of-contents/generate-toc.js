f89afd1e0434abf53b67c52dd23ad08cef8a222b
Shiloh Olotu <shiloholotu@gmail.com>
2023-05-12 22:28:14 -0500

    Add 'incomplete' indicator to tocBlocks

--- scripts/learn/table-of-contents/generate-toc.js
+++ scripts/learn/table-of-contents/generate-toc.js
@@ -6,6 +6,7 @@ function generateTOC(){
         table.innerHTML += `
         <a href="learn#introduction/${i}">
             <div class='tocBlock'>
+                <p style='opacity:.5'><img src='minus.svg'>Incomplete</p>
                 <h3>${docs["introduction"][i][0]}</h3>
                 <p> Last Updated: ${docs["introduction"][i][2]}</p>
             </div>
