128f687fdf962543af1eb6ab3597310fc007ba91
Shiloh Olotu <shiloholotu@gmail.com>
2023-05-20 13:50:40 -0500

    Feat: Allow '$' to be used for inline MathJax

--- scripts/markdown-reading/markdown.js
+++ scripts/markdown-reading/markdown.js
@@ -1,13 +1,25 @@
 let converter = new showdown.Converter();
 
 
-
-function formatSpaces(input){
-    return input.replace("\n\n","\n<br>");
+//makes in line MathJax a little easier
+//uses $...$ instead of \(...\)
+function formatMath(input){
+    let dollarSignCount = 0;
+    let ouput = "";
+    for(let c of input){
+        if(c == "$"){
+            if(dollarSignCount % 2 == 0)output += "\\(";
+            else output += "\\)";
+            dollarSignCount++;
+        }
+        else output += c;
+        
+    }
+    return output;
 }
 
 
 function mdToHTML(text){
-    //text = formatSpaces(text);
+    text = formatMath(text);
     return converter.makeHtml(text);
 }
