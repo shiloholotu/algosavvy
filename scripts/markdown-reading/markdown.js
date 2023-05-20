22554bbf5eb885e100d37f58dee66013237aaffa
Shiloh Olotu <shiloholotu@gmail.com>
2023-05-20 14:01:45 -0500

    Fix: Add enough backslashes on math formatting

--- scripts/markdown-reading/markdown.js
+++ scripts/markdown-reading/markdown.js
@@ -8,8 +8,8 @@ function formatMath(input){
     let output = "";
     for(let c of input){
         if(c == "$"){
-            if(dollarSignCount % 2 == 0)output += "\\(";
-            else output += "\\)";
+            if(dollarSignCount % 2 == 0)output += "\\\\(";
+            else output += "\\\\)";
             dollarSignCount++;
         }
         else output += c;
