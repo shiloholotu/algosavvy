d66662aee4d46a9d292fa35fde34fd35b8e831f4
Shiloh Olotu <shiloholotu@gmail.com>
2023-05-29 18:46:42 -0500

    Feat: Generate answer choice buttons when displaying problem

--- scripts/play/play-session.js
+++ scripts/play/play-session.js
@@ -2,4 +2,19 @@ function displayRandomProblem(){
     const problem = generateProblem(gameMode);
     renderMd(problem[0][0],"problemView");
 
+    const choiceContainer = document.createElement("div");
+    
+
+    if(gameMode != "solution-search" && gamMode != "error-blitz"){
+        choiceContainer.style["display"] = "flex";
+        for(let i = 0; i < 4; i++){
+            let marginStyle = "10px";
+            if(i == 3){
+                marginStyle = "0";
+            }
+            choiceContainer.innerHTML += `<button class="answerChoice" style="margin-right:${marginStyle}">${problem[1][i]}</button>;`
+        }
+        document.getElementById("answerSection").appendChild(choiceContainer);
+    }
+
 }
