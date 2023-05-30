fb8824ca8cfcc6bdbafa9310337cdad7d7ee1f57
Shiloh Olotu <shiloholotu@gmail.com>
2023-05-30 13:02:25 -0500

    Feat: Add textarea to answer section

--- scripts/play/play-session.js
+++ scripts/play/play-session.js
@@ -4,16 +4,17 @@ function displayRandomProblem(){
     document.getElementById("answerSection").innerHTML = "";
     renderMd(problem[0][0],"problemView");
 
+    //difficulty indicator color based on difficulty
     document.getElementById("diffIndicator").innerHTML = problem[2][0].toUpperCase() + problem[2].slice(1);
     if(problem[2] == "easy")document.getElementById("diffIndicator").style["background"] = "var(--pretty-green)";
     else if(problem[2] == "medium")document.getElementById("diffIndicator").style["background"] = "var(--pretty-yellow)";
     else if(problem[2] == "hard")document.getElementById("diffIndicator").style["background"] = "var(--pretty-red)";
     else document.getElementById("diffIndicator").style["background"] = "var(--pretty-purple)";
 
-    const choiceContainer = document.createElement("div");
     
-
     if(gameMode != "solution-search" && gameMode != "error-blitz"){
+
+        const choiceContainer = document.createElement("div");
         choiceContainer.style["display"] = "flex";
         //generate answer choice buttons - solution search and error blitz are free response modes
         for(let i = 0; i < 4; i++){
@@ -29,6 +30,10 @@ function displayRandomProblem(){
         }
         document.getElementById("answerSection").appendChild(choiceContainer);
         MathJax.typeset();//for rendering math in time crunch answers
+    }
+
+    else{
+        document.getElementById("answerSection").innerHTML = "<div id='answerTextareaContainer'><textarea id='answerTextarea'></textarea></div>";
     }
 
 }
