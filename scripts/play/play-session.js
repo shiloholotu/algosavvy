c956bc45807735e02ca0bd6231f0b4877c3ab3a2
Shiloh Olotu <shiloholotu@gmail.com>
2023-05-31 13:24:31 -0500

    Feat: Add functionality to answer choice buttons

--- scripts/play/play-session.js
+++ scripts/play/play-session.js
@@ -1,4 +1,3 @@
-
 function displayRandomProblem(){
     const problem = generateProblem(gameMode);
     
@@ -17,7 +16,7 @@ function displayRandomProblem(){
 
         const choiceContainer = document.createElement("div");
         choiceContainer.style["display"] = "flex";
-        
+
         for(let i = 0; i < 4; i++){
 
             let marginStyle = "10px";
@@ -27,7 +26,12 @@ function displayRandomProblem(){
 
             let choiceText = problem[1][i];
             if(gameMode == "time-crunch") choiceText = "\\(" + choiceText + "\\)";//because time crunch answer choices use MathJax
-            choiceContainer.innerHTML += `<button class="answerChoice" style="margin-right:${marginStyle}">${choiceText}</button>`;
+
+            let answerType = "incorrectAnswer";
+            if(problem[1][i] == problem[0][2] && gameMode == "method-madness")answerType = "correctAnswer";
+            if(problem[1][i] == problem[0][3] && gameMode == "time-crunch") answerType = "correctAnswer";
+
+            choiceContainer.innerHTML += `<button onclick="checkAnswer(${i})" class="answerChoice ${answerType}" style="margin-right:${marginStyle}">${choiceText}</button>`;
         }
         document.getElementById("answerSection").appendChild(choiceContainer);
         MathJax.typeset();//for rendering math in time crunch answers
@@ -41,4 +45,35 @@ function displayRandomProblem(){
         <button id="answerDoneButton">Done</button>`;
     }
 
+}
+
+function checkAnswer(answer){
+
+    document.getElementById("correctionSection").innerHTML = "";
+    document.getElementById("correctionSection").style["display"] = "block";
+    
+    const buttons = document.getElementsByClassName("answerChoice");
+    if(buttons[answer].classList.contains("correctAnswer")){//picked right answer
+
+        buttons[answer].style["background"] = "var(--pretty-green)";
+
+        document.getElementById("correctionSection").innerHTML = `
+        <p id="correctionIndicator" style='background:var(--pretty-green)'>
+            <img src="assets/svg/check.svg">Correct
+            <button>No, I was incorrect</button>
+        </p>
+        `
+    }
+
+    else{//picked wrong answer
+
+        buttons[answer].style["background"] = "var(--lighter-border-color)";
+
+        document.getElementById("correctionSection").innerHTML = `
+        <p id="correctionIndicator" style='background:var(--pretty-red)'>
+            <img src="assets/svg/minus.svg">Correct
+            <button>No, I was right</button>
+        </p>
+        `
+    }
 }
