4103491f0c551065885975abd96511dc0710d5fb
Shiloh Olotu <shiloholotu@gmail.com>
2023-06-13 01:22:41 -0500

    Style: hide diff indicator on catch case

--- scripts/play/problem-view.js
+++ scripts/play/problem-view.js
@@ -23,6 +23,7 @@ async function viewProblem(folder,ind){
     }catch(error){
         renderMd(`# Uh Oh🫤\nWe can't find that problem. **Sorry**!\n\nHere's the error: \`${error}\``,"problemView");
         console.error(error);
+        document.getElementById("diffIndicator").style["display"] = "none";
         hideLoadingScreen();
         return;
     }
