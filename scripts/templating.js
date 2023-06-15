1f887b15d44761367707f04bec7d592e829cb013
Shiloh Olotu <shiloholotu@gmail.com>
2023-06-14 21:37:46 -0500

    Feat: Add footer

--- scripts/templating.js
+++ scripts/templating.js
@@ -15,6 +15,16 @@ function renderNavbar(){
     ` + body.innerHTML;
 }
 
+//footer
+function renderFooter(){
+    const body = document.getElementsByTagName("body")[0];
+    body.innerHTML += 
+    `<div id="footer">
+        <p>Contact: <a href="mailto:shiloholotu@gmail.com">shiloholotu@gmail.com</a><span style="color:black;text-shadow: 0 0 2px;">🤙</span></p>
+        <p>Licensed under the <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPL-3.0 License</a></p>
+    </div>`;
+}
+
 //loading screen
 function hideLoadingScreen(){
     document.getElementById("loadingScreen").style["pointer-events"] = "none";
