530f0c87759f62d94acdd0c6f3cb6e317dd27921
Shiloh Olotu <shiloholotu@gmail.com>
2023-06-14 21:10:59 -0500

    Feat: link back to problemset in side bar of problem view page

--- scripts/problemset/problem-view.js
+++ scripts/problemset/problem-view.js
@@ -86,10 +86,9 @@ async function viewProblem(folder,ind){
 
     //side bar links
     document.getElementById("problemName").textContent = problemFiles[folder][ind][0];
-    document.getElementById("problemDiff").textContent = folder[0].toUpperCase() + folder.slice(1);
 
     document.getElementById("problemName").setAttribute("href",`problem#${folder}/${ind}`);
-    document.getElementById("problemDiff").setAttribute("href","contents");
+    document.getElementById("backToProblemset").setAttribute("href","problemset");
 
     displayProbNavBar();
 
