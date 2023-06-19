954bc77fed611f351db2f3225741760766529acf
Shiloh Olotu <shiloholotu@gmail.com>
2023-06-19 11:31:38 -0500

    Style: Use different highlight.js theme

--- scripts/effects/color-scheme.js
+++ scripts/effects/color-scheme.js
@@ -2,8 +2,10 @@
 if(localStorage.getItem("colorScheme") == "system"){
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.getElementById("darkMode").setAttribute("href","styles/dark.css");
+        document.getElementById("codeStyle").setAttribute("href","https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/atom-one-dark-reasonable.min.css");
     }
 }
 else if(localStorage.getItem("colorScheme") == "dark"){
     document.getElementById("darkMode").setAttribute("href","styles/dark.css");
+    document.getElementById("codeStyle").setAttribute("href","https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/atom-one-dark-reasonable.min.css");
 }
