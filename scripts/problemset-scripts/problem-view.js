25688c8106ad2756f23c6c85ebcd5b290714451f
Shiloh Olotu <shiloholotu@gmail.com>
2023-07-06 00:37:54 -0400

    Test: remove confetti from problem view

--- scripts/problemset-scripts/problem-view.js
+++ scripts/problemset-scripts/problem-view.js
@@ -3,12 +3,7 @@ let curFolder = null;
 
 
 //confetti
-let confetti = new Confetti('footer');
-confetti.setCount(75);
-confetti.setSize(1);
-confetti.setPower(25);
-confetti.setFade(false);
-confetti.destroyTarget(false);
+
 
 //adds funcitonality to a tags in doc nav bar
 function displayProbNavBar(){
