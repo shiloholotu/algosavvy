a73ea70e7f3345eac5317614612dc2f20f40d8de
Shiloh Olotu <shiloholotu@gmail.com>
2023-08-05 13:42:45 -0500

    Docs: move building blocks tutorial to easy section

--- scripts/tutorial-scripts/list-of-docs.js
+++ scripts/tutorial-scripts/list-of-docs.js
@@ -4,10 +4,11 @@ const tutorialFiles = {
         ["A Warm Hello: What is AlgoSavvy?", "a-warm-hello.md","05/23/2023"],
         ["Lost in Translation: Picking a Language", "lost-in-translation.md","05/11/2023"],
         ["The Big Ol' O: Space and Time Complexity", "the-big-ol-o.md","05/11/2023"],
-        ["Building Blocks: Ints, Strings, Arrays, and Others", "building-blocks.md","07/05/2023"],
         ["Giving a Helping Hand: Contributing to AlgoSavvy", "giving-a-helping-hand.md","07/05/2023"]
     ],
-    "easy":[],
+    "easy":[
+        ["Building Blocks: Ints, Strings, Arrays, and Others", "building-blocks.md","08/05/2023"]
+    ],
     "medium":[],
     "hard":[],
     "advanced":[]
