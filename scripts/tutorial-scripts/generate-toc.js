0fd38dbefea63c302fe0075789af0e3a2ac356e2
Shiloh Olotu <shiloholotu@gmail.com>
2023-08-05 13:42:02 -0500

    Feat: automate toc generation

--- scripts/tutorial-scripts/generate-toc.js
+++ scripts/tutorial-scripts/generate-toc.js
@@ -1,28 +1,35 @@
-function generateTOC(){
-    const table = document.getElementById("tableOfContents")
-    table.innerHTML = "";
-    table.innerHTML += "<h2>INTRODUCTION</h2>";
-    for(let i in tutorialFiles["introduction"]){
+function generateSection(section){
+    html = `<h2>${section.toUpperCase()}</h2>`;
+    for(let i in tutorialFiles[section]){
 
         //completion status
         let compStatusStr = "<p style='font-weight:lighter'><img src='assets/svg/minus.svg'><span style='opacity:.5'>Incomplete</span></p>";
         let boxShadowStr = "";
-        if(getCompletionStatus(tutorialFiles["introduction"][i][0]) == "complete"){
+        if(getCompletionStatus(tutorialFiles[section][i][0]) == "complete"){
             compStatusStr = "<p style='font-weight:lighter'><img src='assets/svg/check.svg' style='background:var(--pretty-green);border:none'><span style='opacity:.5'>Complete</span></p>";
             boxShadowStr = "style='box-shadow:none'";
         }
-        table.innerHTML += `
-        <a href="learn#introduction/${i}">
+        html += `
+        <a href="learn#${section}/${i}">
             <div class='tocBlock' ${boxShadowStr}>
                 ${compStatusStr}
-                <h3>${tutorialFiles["introduction"][i][0]}</h3>
-                <p style="opacity: .7"> Last Updated: ${tutorialFiles["introduction"][i][2]}</p>
+                <h3>${tutorialFiles[section][i][0]}</h3>
+                <p style="opacity: .7"> Last Updated: ${tutorialFiles[section][i][2]}</p>
             </div>
             
         </a>`;
-        if(i != tutorialFiles["introduction"].length-1){
+        if(i != tutorialFiles[section].length-1){
             table.innerHTML += "<div class='tocBlockSeparator'></div>";
         }
+    }
+
+    return html;
+}
+function generateTOC(){
+    const table = document.getElementById("tableOfContents")
+    table.innerHTML = "";
+    for(let i in ["introduction","easy","medium","hard","advanced"]){
+        table.innerHTML += generateSection(i);
     }
 
 }
