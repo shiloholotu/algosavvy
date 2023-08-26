5d7e7bac42ff0c6139e143e7e1333ede20744607
Shiloh Olotu <shiloholotu@gmail.com>
2023-08-26 17:30:55 -0500

    Style: revamp checkbox in table of contents

--- scripts/problemset-scripts/generate-toc.js
+++ scripts/problemset-scripts/generate-toc.js
@@ -8,10 +8,10 @@ function generateProblemList(diff){
     for(let i in problemFiles[diff]){
 
         //completion status
-        let compStatusStr = "<p style='font-weight:lighter'><img src='assets/svg/minus.svg'><span style='opacity:.5'>Incomplete</span></p>";
+        let compStatusStr = "<p style='font-weight:lighter'><div class='checkBox' style='margin-right:10px'><div style='background:var(--light-transp-blue)'></div></div><span style='opacity:.5'>Incomplete</span></p>";
         let boxShadowStr = "";
         if(getCompletionStatus(problemFiles[diff][i][0]) == "complete"){
-            compStatusStr = "<p style='font-weight:lighter'><img src='assets/svg/check.svg' style='background:var(--pretty-green);border:none'><span style='opacity:.5'>Complete</span></p>";
+            compStatusStr = "<p style='font-weight:lighter'><div class='checkBox' style='margin-right:10px'><div style='background:var(--pretty-green)'></div></div><span style='opacity:.5'>Complete</span></p>";
             boxShadowStr = "style='box-shadow:none'";
         }
 
